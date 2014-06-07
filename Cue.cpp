7d5444e3316c637ad452e1965b988196615c74d6
kagami-c <kagami.cathez@gmail.com>
2014-06-07 14:18:56 +0800

    draw cue and change front sight style

--- Cue.cpp
+++ Cue.cpp
@@ -31,16 +31,36 @@ void Cue::Update(int gameState, Vector2 mousePosition)
 
 void Cue::Draw(QPainter& painter, Ball& cueBall)
 {
-	// Draw here
-	QColor frontSightColor(255, 255, 255, 200);
-	painter.setPen(frontSightColor);
+	// Draw front sight 
+	QColor frontSightColor(200, 200, 200, 100);
+
+    QPen pen;
+    pen.setColor(QColor(50, 50, 50, 200));
+    pen.setStyle(Qt::DashLine);
+    painter.setPen(pen);
+
 	painter.setBrush(frontSightColor);
     painter.drawEllipse(QPoint(mousePosition.getX(), mousePosition.getY()), ballRadius, ballRadius);
 
     Vector2 cuePosition = cueBall.getPosition();
-    QColor lineColor(0, 0, 0);
-    painter.setPen(lineColor);
-	painter.drawLine(cuePosition.getX(), cuePosition.getY(), mousePosition.getX(), mousePosition.getY());
+
+    Vector2 direction = mousePosition - cuePosition;
+    direction = direction.getNormalize();
+    Vector2 offset = direction * cueBall.getRadius();
+    Vector2 beginPosition = cuePosition + offset;
+    Vector2 endPosition = mousePosition - offset;
+
+	painter.drawLine(beginPosition.getX(), beginPosition.getY(), endPosition.getX(), endPosition.getY());
+
+	// draw the cue
+	offset = offset * (-3);
+	endPosition = cuePosition + offset;
+	offset = offset * 8;
+	beginPosition = cuePosition + offset;
+	QColor cueColor(143, 100, 69);
+	painter.setPen(QPen(cueColor, 12, Qt::SolidLine, Qt::RoundCap));
+	painter.drawLine(beginPosition.getX(), beginPosition.getY(), endPosition.getX(), endPosition.getY());
+
 }
 
 void Cue::Stroke(int elapsed, Ball& cueBall)
